b603b919dd845e076d286b30124e251779bf4e14
Wayne Yeap <60493677+fdx-wayne-yeap@users.noreply.github.com>
2025-04-22 09:49:33 +1000

    feat: whichkey, git stuff, grug and treesitter bindings

---
+++ lua/plugins/git.lua
@@ -0,0 +1,12 @@
+return {
+    {
+        "lewis6991/gitsigns.nvim",
+        opts = {}
+    },
+    {
+        "sindrets/diffview.nvim",
+    },
+    {
+        "tommcdo/vim-fubitive",
+    }
+}
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,2 +1,30 @@
 return {
+    {
+        "nvim-treesitter/nvim-treesitter",
+        opts = {
+            textobjects = {
+                select = {
+                    enable = true,
+
+                    -- Automatically jump forward to textobj, similar to targets.vim
+                    lookahead = true,
+
+                    keymaps = {
+                        -- You can use the capture groups defined in textobjects.scm
+                        ["af"] = "@function.outer",
+                        ["if"] = "@function.inner",
+                        ["ac"] = "@class.outer",
+                        -- You can optionally set descriptions to the mappings (used in the desc parameter of
+                        -- nvim_buf_set_keymap) which plugins like which-key display
+                        ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
+                        -- You can also use captures from other query groups like `locals.scm`
+                        ["as"] = { query = "@local.scope", query_group = "locals", desc = "Select language scope" },
+                    }
+                }
+            }
+        },
+        config = function(_, opts)
+            require("nvim-treesitter.configs").setup(opts)
+        end,
+    }
 }
